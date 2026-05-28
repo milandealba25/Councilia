9b3a1431d011fd673023bd86b498caaa9224615c
Milan <milanalba@outlook.com>
2026-05-28 14:08:22 -0600

    add speed insights (performance metrics)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import localFont from "next/font/local";
 import "./globals.css";
 import { CookieBanner } from "@/components/cookies/CookieBanner";
